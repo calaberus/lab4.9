eb0551d438b8303b2fb94ce01c85d94f92564a51
calaberus <juan.esteban.mondragon@correounivalle.edu.co>
2024-10-03 10:42:43 -0500

    Add files via upload

---
+++ interfase publica de funcionalidades 9.py
@@ -0,0 +1,19 @@
+class Temporizador:
+
+   def iniciar(self,tiempo_inicial):
+       self.tiempo_restante=tiempo_inicial
+   
+   def iniciar(self):
+       print(f"temporizador iniciando con{self.tiempo_restante}segundos.")
+       
+   def pausar(self):
+       print(f"temporizador pausado con{self.tiempo_restante}segubdos restantes.")
+       
+       
+def mostar_tiempo(self):
+    print(f"Tiemporestante:{self.tiempo_restante}segundos.")
+         
+        
+        
+       
+       
